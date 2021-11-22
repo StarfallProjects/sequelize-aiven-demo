39b6051ac6d5edc14d0f693918d1c73ce7bdb47d
Deborah Barnard <deborah@starfallprojects.co.uk>
2021-11-22 22:30:48 +0000

    massively simplify example

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ This repo contains example code and a tutorial. It introduces basic Sequelize fu
 2. Install the dependencies with `npm install`.
 3. Follow the [tutorial](./TUTORIAL.md) to set up your Aiven database service.
 4. Create a `.env` file and add your Aiven Service URI.
-5. Run `npm run bootstrap` to add example data to your new database.
-6. Use `npm start` to run the examples in [index.js](./index.js).
+5. Run `npm start` to add example data to your new database and run the examples.
+
--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // Create the connection by passing a connection URI
 const sequelize = new Sequelize(process.env.AIVEN_MYSQL_URI)
 
+// define the 'Book' model
+const Book = sequelize.define('Book', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    authorName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+// Create the 'Books' table and add some data
+async function createBooksTable() {
+    let res = await Book.sync();
+    if(Book.count !=0) {
+        console.log("Database has rows already!")
+        // Run examples
+        getAllBooks();
+    } else {
+        console.log("Adding some rows . . .");
+        res.bulkCreate([
+            {title: "Carpe Jugulum", authorName: "Terry Pratchett"},
+            {title: "Hold Your Own", authorName: "Kae Tempest"},
+            {title: "Neuromancer", authorName: "William Gibson"}
+        ]);
+        // Run examples
+        getAllBooks();
+    }  
+}
+createBooksTable();
+
+// List all books
+async function getAllBooks() {
+    const books = await Book.findAll();
+    console.log("All books:", JSON.stringify(books, null, 2));
+}
 
 
 
